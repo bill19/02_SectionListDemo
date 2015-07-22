83eac6a9aea43a038513d92b68e8912461d4c6c1
bill19 <sunhao.private@foxmail.com>
2015-07-22 15:08:55 +0800

    Initial Commit

---
+++ 02_SectionListDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  02_SectionListDemo
+//
+//  Created by sunhao on 15-7-22.
+//  Copyright (c) 2015年 SH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 02_SectionListDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  02_SectionListDemo
+//
+//  Created by sunhao on 15-7-22.
+//  Copyright (c) 2015年 SH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
